8551fd2afe00f1a1423fad7570703a43cc58b2d2
Vikas Singh <xanderbilla@gmail.com>
2023-02-27 20:05:00 +0200

    Added Homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Slider from '../../components/Slider/Slider'
 
@@ -9,7 +11,9 @@ const Home = () => {
     <div className="home">
       <Slider />
       <FeaturedProducts type="featured" />
+      <Categories/>
       <FeaturedProducts type="trending" />
+      <Contact/>
     </div>
   )
 }
