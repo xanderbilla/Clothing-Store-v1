06868638e4a021afbf91d9f7527bf0065fdd5668
Vikas Singh <xanderbilla@gmail.com>
2023-02-27 20:03:14 +0200

    Added Card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,10 +1,23 @@
 import './Card.scss'
 import React from 'react'
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom'
 
 const Card = ({ item }) => {
     return (
-        <div></div>
+        <Link className='link' to={`/product/${item.id}`}>
+            <div className='card'>
+                <div className="image">
+                    {item.isNew && <span>New Season</span>}
+                    <img src={item.img} alt="" className="main-img" />
+                    <img src={item.img2} alt="" className="second-img" />
+                </div>
+                <h2>{item.title}</h2>
+                <div className="prices">
+                    <h3>${item.oldPrice}</h3>
+                    <h3>${item.price}</h3>
+                </div>
+            </div>
+        </Link>
     )
 }
 
